648d1e73e8696b348ac8432c1b6a86fbf0dd8c7d
shaqvendetta <shaquillemartin16@yahoo.com>
2018-03-05 03:37:00 +0000

    fourth commit

--- app/views.py
+++ app/views.py
@@ -27,6 +27,14 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    """Render a secure page on our website that only logged in users can access."""
+    return render_template('secure_page.html')
+    
+    
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
